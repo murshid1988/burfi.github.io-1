b7bb1564115bf053d57a14824aca21289cd0650d
Murshid <murshid@platform.lk>
2016-09-11 15:30:46 +0530

    Added and enhancement

--- js/app.js
+++ js/app.js
@@ -1,1 +1,29 @@
-$(document).ready(function(){localStorage.noteText&&$("#note").val(localStorage.noteText),$("#note").keyup(function(){localStorage.noteText=$("#note").val();var t=localStorage.getItem("noteText");$(".dropbox-saver").attr("href","data:,"+t)}),$(window).resize(function(){$("#note").css("left",$(window).width()/2-$("#note").width()/2-20+"px"),$("#note").css("top",$(window).height()/2-$("#note").height()/2-20+"px")}),$(window).resize()}),$(document).ready(function(){var t=localStorage.getItem("noteText");$(".dropbox-saver").attr("href","data:,"+t)});
+$(document).ready(function() {
+    localStorage.noteText && $("#note").val(localStorage.noteText), 
+    
+    $("#note").keyup(function() {
+        localStorage.noteText = $("#note").val();
+        var ntTxt = localStorage.getItem('noteText');
+        if(ntTxt.length<1) {
+            $('.dropbox-saver').attr('href','data:,You just saved an empty file');
+        } else {
+            $('.dropbox-saver').attr('href','data:,'+ ntTxt);
+        } 
+    }), 
+    
+    $(window).resize(function() {
+        $("#note").css("left", $(window).width() / 2 - $("#note").width() / 2 - 20 + "px"), $("#note").css("top", $(window).height() / 2 - $("#note").height() / 2 - 20 + "px")
+    }),  
+    
+    $(window).resize()
+
+});
+
+$(document).ready(function() {
+    var ntTxt = localStorage.getItem('noteText');
+    if(ntTxt.length<1) {
+        $('.dropbox-saver').attr('href','data:,You just saved an empty file');
+    } else {
+        $('.dropbox-saver').attr('href','data:,'+ ntTxt);
+    } 
+});
